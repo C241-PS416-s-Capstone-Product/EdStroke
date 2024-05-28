60054d36bc78aa698caffd90bc9072c83f4b20bc
rafiadiwibowo <muhammad.rafi15@ui.ac.id>
2024-05-28 23:53:41 +0700

    Update db

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const db = mysql.createConnection({
-    host: '34.128.99.134',
-    user: 'root',
-    database: 'auth_db'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 db.connect(err => {
